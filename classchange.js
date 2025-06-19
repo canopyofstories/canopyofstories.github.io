e7cf61c1288c1574ab0d485605ee8b07e680e589
Nicole Huynh <paperpothos@gmail.com>
2025-06-19 14:58:10 +1000

    upload 2nd time

---
+++ classchange.js
@@ -0,0 +1,21 @@
+const classChanges = [
+
+  { time: 1, target: ".p1", add: "fade-in" },
+  { time: 16, target: ".p2", add: "fade-in" },
+  { time: 16, target: ".p3", add: "fade-in" },
+  { time: 115, target: "disappeartext", add: "fade-out" },
+  { time: 363, target: ".imgsplit", add: "fade-in" },
+];
+  /*
+  108
+  { time: 6, target: ".p1", remove: "highlight" },
+  { time: 6, target: ".p2", add: "highlight" },
+
+  { time: 8, target: ".p2", remove: "highlight" },
+  { time: 8, target: ".p3", add: "highlight" },  
+
+
+  { time: 10, target: ".p3", remove: "highlight" },
+  { time: 10, target: ".p4", add: "highlight" }, */
+
+
